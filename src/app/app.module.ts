4cc1eb88af1ed20eec97931fea792b18368cea87
Gustavo Domingueti <gustavo.s.domingueti@gmail.com>
2021-11-04 18:51:58 -0300

    feat: instala Angular Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { APP_BASE_HREF } from '@angular/common';
 
 import { CoreModule } from './core/core.module';
@@ -10,7 +11,7 @@ import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, CoreModule, AppRoutingModule],
+  imports: [BrowserModule, BrowserAnimationsModule, CoreModule, AppRoutingModule],
   providers: [{ provide: APP_BASE_HREF, useValue: '/' }],
   bootstrap: [AppComponent],
 })
