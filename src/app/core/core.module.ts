82197b0f27f0c4fbfd34653815d79f4fac43ffdc
Gustavo Domingueti <gustavo.s.domingueti@gmail.com>
2021-11-04 20:30:07 -0300

    chore: importa AccountService e TaskService nos providers em CoreModule

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,8 +2,12 @@ import { NgModule } from '@angular/core';
 
 import { LayoutsModule } from './layouts/layouts.module';
 
+import { AccountService } from './services/account.service';
+import { TaskService } from './services/task.service';
+
 @NgModule({
   imports: [LayoutsModule],
   exports: [LayoutsModule],
+  providers: [AccountService, TaskService],
 })
 export class CoreModule {}
