ce01d3b662dc17e7fce2bcaadf5a745bccd4ae06
Gustavo Domingueti <gustavo.s.domingueti@gmail.com>
2021-11-04 11:46:20 -0300

    chore: remove karma e adiciona e configura o jest para os testes

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  preset: 'jest-preset-angular',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/dist/'],
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+      stringifyContentPathRegex: '\\.html$',
+    },
+  },
+};
