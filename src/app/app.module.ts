64d342d26b3932b0975210103a880710409ee084
devcastrojunin <devcastrojunin@gmail.com>
2021-11-11 10:46:19 -0300

    Import routing module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AuthComponent } from './layouts/auth/auth.component';
+import { DashboardComponent } from './layouts/dashboard/dashboard.component';
+import { LoginComponent } from './pages/login/login.component';
+import { TasksComponent } from './pages/tasks/tasks.component';
+import { AccountComponent } from './pages/account/account.component';
+import { AppRoutingModule } from './app-routing.module';
 @NgModule({
   declarations: [	
-    AppComponent,
+    AppComponent, AuthComponent, DashboardComponent, LoginComponent, TasksComponent, AccountComponent, AccountComponent,
    ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
