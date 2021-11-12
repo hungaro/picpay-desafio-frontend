7d5006c90f6d2967208437a061e62a82ea20f0dd
Raphael Wanderley <rapharw@users.noreply.github.com>
2021-11-12 16:37:07 -0300

    feat(lib): add @fortawesome/angular-fontawesome
    add @fortawesome/angular-fontawesome to use many icons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 @NgModule({
   declarations: [	
     AppComponent,
    ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
