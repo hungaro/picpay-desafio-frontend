48e155ac842bba8f93897da7237e94ae42551fe0
Raphael Wanderley <rapharw@users.noreply.github.com>
2021-11-13 18:15:02 -0300

    feat(my-payments): add initial my-payments structure

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
+import { MyPaymentsComponent } from './payments/my-payments/my-payments.component';
 
 const components = [
-  LoginComponent
+  LoginComponent, MyPaymentsComponent
 ]
 
 @NgModule({
