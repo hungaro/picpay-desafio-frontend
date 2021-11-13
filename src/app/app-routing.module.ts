e8b9c500c96418c8b6773cdcbaa495d7028a2fe6
Raphael Wanderley <rapharw@users.noreply.github.com>
2021-11-13 18:31:36 -0300

    feat(errors): add errors components
    add errors components in case not found or access denied

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AccessDeniedComponent } from './core/errors/access-denied/access-denied.component';
+import { NotFoundComponent } from './core/errors/not-found/not-found.component';
 import { LoginComponent } from './pages/login/login.component';
 import { MyPaymentsComponent } from './pages/payments/my-payments/my-payments.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'my-payments', component: MyPaymentsComponent },
+  {
+    path: 'denied',
+    component: AccessDeniedComponent,
+  },
+  { path: '**', component: NotFoundComponent },
 ];
 
 @NgModule({
