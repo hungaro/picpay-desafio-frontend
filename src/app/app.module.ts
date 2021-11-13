e9556dad77544df3ded60c2e663bb19adaa0fb16
Raphael Wanderley <rapharw@users.noreply.github.com>
2021-11-13 16:27:08 -0300

    style(structure): add module to use material
    adding module to use material with more flexibility

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
+import { AppComponent } from './app.component';
+import { MaterialModule } from './material.module';
+
 @NgModule({
   declarations: [	
     AppComponent,
@@ -11,7 +13,8 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
