fd65d0d14428fc6f8ec0f08ac50e695578531909
Raphael Wanderley <rapharw@users.noreply.github.com>
2021-11-15 18:49:19 -0300

    style(my-payments): adjust to use page-structure component

--- src/app/components/templates/payments/t-payments.module.ts
+++ src/app/components/templates/payments/t-payments.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { MaterialModule } from '../../../material.module';
 
 import { SharedModule } from '../../shared/shared.module';
+import { PageStructureModule } from '../page-structure/page-structure.module';
 import { TPaymentsComponent } from './t-payments/t-payments.component';
 
 const components = [
@@ -15,7 +15,7 @@ const components = [
   imports: [
     MaterialModule,
     SharedModule,
-    FormsModule, ReactiveFormsModule
+    PageStructureModule
   ],
   exports: components
 })
