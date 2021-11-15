468c6166a49d55fdd60fed641c63584988780abe
devcastrojunin <devcastrojunin@gmail.com>
2021-11-14 22:31:44 -0300

    Ordering payments; Save and Edit methods

--- src/app/shared/services/payment.service.ts
+++ src/app/shared/services/payment.service.ts
@@ -1,4 +1,6 @@
+import { HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { PaymentModel } from '../models/payment.model';
 import { ApiService } from './api.service';
 
 @Injectable({
@@ -9,7 +11,7 @@ export class PaymentService {
   constructor(private apiService: ApiService) { }
 
   fetchPayments(page, total){
-    const endpoint = `tasks?_page=${page}&_limit=${total}`;
+    const endpoint = `tasks?_sort=date&_order=desc&_page=${page}&_limit=${total}`;
     const route = this.apiService.url + endpoint;
 
     return this.apiService
@@ -28,6 +30,30 @@ export class PaymentService {
       .toPromise()
       .then(data => {
         return data.length;
+      });
+  }
+
+  savePayment(payment: PaymentModel){
+    const endpoint = `tasks`;
+    const route = this.apiService.url + endpoint;
+
+    return this.apiService
+      .post$(route, payment)
+      .toPromise()
+      .then(data => {
+        return data;
+      });
+  }
+
+  updatePayment(payment, paymentId){
+    const endpoint = `tasks/${paymentId}`;
+    const route = this.apiService.url + endpoint;
+
+    return this.apiService
+      .update$(route, payment)
+      .toPromise()
+      .then(data => {
+        return data;
       });
   }
 
