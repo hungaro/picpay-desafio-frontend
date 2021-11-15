956b5ac650f6d723eae4a17f89148eeaff21eb56
Raphael Wanderley <rapharw@users.noreply.github.com>
2021-11-15 17:20:20 -0300

    style(app): update gitignore and remove unsuded modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { AppComponent } from './app.component';
-import { MaterialModule } from './material.module';
-import { TemplatesModule } from './components/templates/templates.module';
 import { SharedModule } from './components/shared/shared.module';
 import { AppRoutingModule } from './app-routing.module';
 import { PagesModule } from './pages/pages.module';
@@ -21,8 +19,6 @@ import { CoreModule } from './core/core.module';
     BrowserAnimationsModule,
     HttpClientModule,
     FontAwesomeModule,
-    MaterialModule,
-    TemplatesModule,
     SharedModule,
     AppRoutingModule,
     PagesModule,
