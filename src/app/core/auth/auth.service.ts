fe577ed9fff191356b9c1450ccd1b53ada61cda2
Raphael Wanderley <rapharw@users.noreply.github.com>
2021-11-15 19:19:10 -0300

    feat(logout): add logout service
    add logout service to remove token and redirect to login page

--- src/app/core/auth/auth.service.ts
+++ src/app/core/auth/auth.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { User } from './user';
 import { environment } from '../../../environments/environment';
+import { TokenService } from '../token/token.service';
+import { Router } from '@angular/router';
 
 const API_URL = environment.apiUrl;
 
@@ -9,11 +11,16 @@ const API_URL = environment.apiUrl;
   providedIn: 'root',
 })
 export class AuthService {
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private tokenService: TokenService, private router: Router) {}
 
   authenticate(username: string, password: string) {
     return this.http.get<User[]>(
       `${API_URL}/account?email=${username}&password=${password}`
     );
+  }
+
+  logout(){
+    this.tokenService.removeToken();
+    this.router.navigate(['login']);
   }
 }
